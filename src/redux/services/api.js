948e283ecd1c319fe13f6a3a2ec8b233fb90e4fc
dspasic <danijelspasic@gmail.com>
2020-07-26 23:02:35 +0200

    add redux login, logout, remove api key

--- src/redux/services/api.js
+++ src/redux/services/api.js
@@ -1,4 +1,4 @@
 export const loginURL = "http://dev.api.kabox.io/api/auth/login";
 
-const API_KEY = "d38aa8716411ef7d8e9054b34a6678ac";
+const API_KEY = "";
 export const movieURL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
